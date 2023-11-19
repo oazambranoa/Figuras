142cebe71d5413c90cb1bb3152e9445903aa6820
oazambranoa <oazambranoa@unal.edu.co>
2023-11-18 23:35:21 -0500

    Inicializacion y terminacion del ejercicio

---
+++ src/main/java/com/mycompany/figuras/Figuras.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.figuras;
+
+/**
+ *
+ * @author omarz
+ */
+import javax.swing.JFrame;
+    
+public class Figuras {
+    public static void main(String[] args) {
+        JFrame miVentanaPrincipal;
+        miVentanaPrincipal = new JFrame();
+        miVentanaPrincipal.setVisible(true);
+        miVentanaPrincipal.setResizable(false);
+    }
+}
